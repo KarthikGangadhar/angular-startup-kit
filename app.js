d98d6557ec89617e4bab6fdae7e695de87f6e7ac
kgangadhar <karthikg1643@gmail.com>
2020-03-26 03:43:31 -0500

    added mongodb configuration

--- app.js
+++ app.js
@@ -4,25 +4,30 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/mean-angular5', { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => console.log('connection succesful'))
+  .catch((err) => console.error(err));
+
 var book = require('./routes/book');
 var app = express();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({'extended':'false'}));
+app.use(bodyParser.urlencoded({ 'extended': 'false' }));
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/books', express.static(path.join(__dirname, 'dist')));
 app.use('/book', book);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
